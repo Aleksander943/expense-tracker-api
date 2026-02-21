23ef9bcabea34e845bdbd79894177d876919590b
Aleksander943 <aleksandededeus943@gmail.com>
2026-02-21 11:11:25 -0300

    chore(server): ajustar bootstrap do servidor para ESM

--- src/server.js
+++ src/server.js
@@ -1,4 +1,13 @@
-const app = require("./app");
-const porta = 3000;
 
-app.listen(porta, () =>{console.log("Servidor Rodando na porta" + porta)})
+import "dotenv/config";
+import app from "./app.js"; // O arquivo app.js está na mesma pasta?
+
+const porta = process.env.PORT || 8080;
+
+app.get("/teste-direto", (req, res) => {
+  res.send("O servidor está vivo!");
+});
+
+app.listen(porta, () => {
+  console.log(`Servidor online em: http://localhost:${porta}`);
+});
