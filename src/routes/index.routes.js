c854db4c8934373553a83deb3f0adcc8093507f0
Aleksander943 <aleksandededeus943@gmail.com>
2026-02-21 11:07:59 -0300

    feat(api): adicionar auth JWT, usuários e transações

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,8 +1,25 @@
-const express = require("express");
-const router = express.Router();
+import { Router } from "express";
+import userRoutes from "./user.routes.js";
+import { auth } from "../middlewares/auth.js";
+import transactionController from "../controllers/transaction.controller.js"
+
+const router = Router();
 
 router.get("/", (req, res) => {
   res.json({ mensagem: "API Financeiro funcionando 🚀" });
 });
 
-module.exports = router;
+router.get("/me", auth, (req, res) => {
+  res.json({
+    mensagem: "Rota protegida, usuário autenticado!",
+    seuId: req.userId
+  });
+})
+
+router.post("/transaction", auth, transactionController.transaction);
+
+router.get("/transactions", auth, transactionController.index);
+
+router.use(userRoutes);
+
+export default router;
---
+++ src/routes/user.routes.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import userController from "../controllers/user.controller.js";
+
+const router = Router();
+
+router.post("/users", userController.create);
+router.post("/login", userController.login);
+
+export default router;
