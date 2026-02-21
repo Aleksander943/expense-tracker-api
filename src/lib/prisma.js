5911a52843196e0ab4867466274a9acc0971b2dc
Aleksander943 <aleksandededeus943@gmail.com>
2026-02-21 16:37:49 -0300

    refactore: correção do arquivo

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -6,10 +6,7 @@ const pool = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
-  },
-  max: 20, // Limite de conexões
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
+  }
 });
 const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({ adapter });
