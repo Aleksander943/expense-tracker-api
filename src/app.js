aeb8afbc02f75fe69f3ba4882216db2053272282
Aleksander943 <aleksandededeus943@gmail.com>
2026-02-21 14:36:16 -0300

    feat: permitir comunicação entre back e front

--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
-// app.js
 import express from "express";
+import cors from "cors";
 import router from "./routes/index.routes.js";
 
 const app = express();
 app.use(express.json());
+
+app.use(cors({
+  origin: "*", // Permite qualquer origem (ideal para desenvolvimento)
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
+
 app.use(router);
+
 
 export default app; // Se faltar isso, o server.js não funciona.
