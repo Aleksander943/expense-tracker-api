bbd4482a552340dfdb409923d52a3a6be114c7f8
Aleksander943 <aleksandededeus943@gmail.com>
2026-02-21 14:37:09 -0300

    refactor: improve transaction controller implementation

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -26,7 +26,51 @@ const transactionController = {
             where:{userId:Number(userId)}
         });
         return res.json(transaction)
+    },
+
+    async delete(req,res){
+        const {id} = req.params;
+        const userId = req.userId
+        try{
+            await prisma.transaction.deleteMany({
+                where: {
+                    id: Number(id),
+                    userId: Number(userId)
+                }
+            });
+            return res.status(204).send()
+        }catch (error){
+            return res.status(500).json({ error:"Erro ao deletar transação" });
+        }
+    },
+
+    async put(req, res){
+        const {id} = req.params;
+        const userId = req.userId;
+        const {description,value,type} = req.body
+        try{
+            const result = await prisma.transaction.updateMany({
+                where:{
+                    id: Number(id),
+                    userId: Number(userId)
+                },
+                data: {
+                    description,
+                    value: Number(value),
+                    type
+                }
+            });
+
+            if (result.count === 0) {
+            return res.status(404).json({ error: "Transação não encontrada" });
+        }
+
+            return res.status(200).json({mensagem:" Atualizado com sucesso"})
+        }catch (error){
+            return res.status(500).json({error:"Erro ao editar transação"})
+        }
     }
+    
 };
 
 export default transactionController;
