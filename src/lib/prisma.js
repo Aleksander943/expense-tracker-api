57aa89b24363bc7ebae160a31d91596b8164d60c
Aleksander943 <aleksandededeus943@gmail.com>
2026-02-21 16:16:04 -0300

    feat: alteração do caminho

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -2,7 +2,12 @@ import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 import pg from 'pg';
 
-const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
+const pool = new pg.Pool({ 
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false // <--- ESSA LINHA É A CHAVE!
+  }
+});
 const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({ adapter });
 
