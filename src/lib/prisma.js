5cf8394f78a88a55e48da760023273b5d90e9770
Aleksander943 <aleksandededeus943@gmail.com>
2026-02-21 16:18:16 -0300

    feat: atualizar caminho

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -5,8 +5,11 @@ import pg from 'pg';
 const pool = new pg.Pool({ 
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false // <--- ESSA LINHA É A CHAVE!
-  }
+    rejectUnauthorized: false
+  },
+  max: 20, // Limite de conexões
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
 });
 const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({ adapter });
