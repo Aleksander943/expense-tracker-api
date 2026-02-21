5f35032639d5bdb6d8ae30dc3512079407b84a1d
Aleksander943 <aleksandededeus943@gmail.com>
2026-02-21 14:38:25 -0300

    feat(rotas): configurar rotas principais da API com autenticação

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import userRoutes from "./user.routes.js";
 import { auth } from "../middlewares/auth.js";
 import transactionController from "../controllers/transaction.controller.js"
+import dashboard from "../controllers/dashboard.controller.js";
 
 const router = Router();
 
@@ -19,6 +20,12 @@ router.get("/me", auth, (req, res) => {
 router.post("/transaction", auth, transactionController.transaction);
 
 router.get("/transactions", auth, transactionController.index);
+
+router.delete("/transaction/:id", auth, transactionController.delete)
+
+router.put("/transaction/:id", auth, transactionController.put)
+
+router.get("/dashboard", auth, dashboard.dashboard)
 
 router.use(userRoutes);
 
