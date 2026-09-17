90388dc64a05beb2bed429f306ec491f5ab00009
Aleksander943 <aleksandededeus943@gmail.com>
2026-09-17 18:50:02 -0300

    feat: implementa controllers de transações

--- src/controllers/transaction.controller.ts
+++ src/controllers/transaction.controller.ts
@@ -1,72 +1,96 @@
 import type { Request, Response } from "express";
-import { CreateTransaction, listTransactions } from "../services/transaction.service.js";
-
+import {
+  CreateTransaction,
+  deleteTransaction,
+  listTransactions,
+  updateTransaction,
+} from "../services/transaction.service.js";
 
 export const createTransactionController = async (
   req: Request,
   res: Response,
 ) => {
-  const {description, value, type, transactionDate} = req.body;
+  const { description, value, type, transactionDate } = req.body;
   const userId = req.userId;
 
-  try{
+  try {
     const transaction = await CreateTransaction({
       userId,
       description,
       value: Number(value),
       type,
-      transactionDate: new Date(transactionDate)
-    })
+      transactionDate: new Date(transactionDate),
+    });
 
-    return res.status(201).json(transaction)
-  }catch(error){
+    return res.status(201).json(transaction);
+  } catch (error) {
     return res.status(500).json({
-      error: "Erro ao criar transação"
-    })
-
+      error: "Erro ao criar transação",
+    });
   }
-
-  
-  // 3. Converter transactionDate para Date
-  // 4. Validar os dados
-  // 5. Chamar createTransaction(...)
-  // 6. Retornar status 201
 };
-
 
 export const listTransactionsController = async (
   req: Request,
   res: Response,
 ) => {
   const userId = req.userId;
-  
-  try{
-    const transactionList = await listTransactions(userId)
-    return res.status(200).json(transactionList)
-  }catch(error){
+
+  try {
+    const transactionList = await listTransactions(userId);
+    return res.status(200).json(transactionList);
+  } catch (error) {
     return res.status(500).json({
-      error: "Erro ao listar transações"
-    })
+      error: "Erro ao listar transações",
+    });
   }
 };
 
-// export const updateTransactionController = async (
-//   req: Request,
-//   res: Response,
-// ) => {
-//   // 1. Converter req.params.id para number
-//   // 2. Pegar e converter os dados do req.body
-//   // 3. Chamar updateTransaction({...})
-//   // 4. Verificar se result.count === 0
-//   // 5. Retornar 404 ou 200
-// };
+export const updateTransactionController = async (
+  req: Request,
+  res: Response,
+) => {
+  const userId = req.userId;
 
-// export const deleteTransactionController = async (
-//   req: Request,
-//   res: Response,
-// ) => {
-//   // 1. Converter req.params.id para number
-//   // 2. Chamar deleteTransaction(id, req.userId)
-//   // 3. Verificar se result.count === 0
-//   // 4. Retornar 404 ou 204
-// };
+  const transactionId: number = Number(req.params.id);
+  const { description, value, type, transactionDate } = req.body;
+
+  const convertedValue = Number(value);
+  const convertedTransactionDate = new Date(transactionDate);
+  try {
+    const transactionUpdate = await updateTransaction({
+      userId,
+      id: transactionId,
+      description,
+      value: convertedValue,
+      type,
+      transactionDate: convertedTransactionDate,
+    });
+
+    if (transactionUpdate.count === 0) {
+      return res.status(404).json({ error: "Transação não identificada" });
+    }
+
+    return res.status(200).json({ mensagem: "Transação atualizada com sucesso." });
+  } catch (error) {
+    return res.status(500).json({ error: "Erro ao atualizar a transação." });
+  }
+};
+
+export const deleteTransactionController = async (
+  req: Request,
+  res: Response,
+) => {
+  const userId = req.userId;
+  const id = Number(req.params.id);
+
+  try {
+    const transactionDelete = await deleteTransaction(id, userId);
+    if (transactionDelete.count === 0) {
+      return res.status(404).json({ error: "Transação não identificada." });
+    }
+    return res.status(204).end();
+  } catch (error) {
+    return res.status(500).json({ error: "Erro ao deletar transação." });
+  }
+};
